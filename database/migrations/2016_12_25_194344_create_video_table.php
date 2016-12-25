2fdfff6aa41d5c39a394b5b9ad352f2a23a01ad3
gazer099 <a3104123@gmail.com>
2016-12-25 22:39:43 +0800

    Create video table

---
+++ database/migrations/2016_12_25_194344_create_video_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('video_id');
+            $table->string('name');
+            $table->unsignedInteger('running_time');
+            $table->string('genre');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('video');
+    }
+}
