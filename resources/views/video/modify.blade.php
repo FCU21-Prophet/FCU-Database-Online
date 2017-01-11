140c01631dc2de132664c1ec70b7fe9f94110c06
PuddingPudding <hwsh70119@gmail.com>
2017-01-11 22:05:33 +0800

    修正 {{}} 忘記加雙引號的警告

--- resources/views/video/modify.blade.php
+++ resources/views/video/modify.blade.php
@@ -7,31 +7,31 @@
 @endsection
 @section('content')
 
-    <form class="ui form" action="{{route('videoStore')}}" method="post">
+    <form class="ui form">
         {{ csrf_field() }}
         <div class="field">
             <label style="color: white">片名</label>
-            <input type="text" name="video_name" value={{ $video['name'] }} placeholder="片片名稱...?">
+            <input type="text" name="video_name" value="{{ $video['name'] }}" placeholder="片片名稱...?">
         </div>
 
         <div class="field">
             <label style="color: white">國家</label>
-            <input type="text" name="country" value={{ $video['country'] }} placeholder="國家名稱...?">
+            <input type="text" name="country" value="{{ $video['country'] }}" placeholder="國家名稱...?">
         </div>
 
         <div class="field">
             <label style="color: white">片長</label>
-            <input type="text" name="running_time" value={{ $video['running_time'] }} placeholder="片長多久...?">
+            <input type="text" name="running_time" value="{{ $video['running_time'] }}" placeholder="片長多久...?">
         </div>
 
         <div class="field">
             <label style="color: white">URL</label>
-            <input type="text" name="URL" value={{ $video['url'] }} placeholder="(youtube only)">
+            <input type="text" name="URL" value="{{ $video['url'] }}" placeholder="(youtube only)">
         </div>
 
         <div class="field">
             <label style="color: white">簡介</label>
-            <input type="text" name="introduction" value={{ $video['introduction'] }} placeholder="片片在講啥...?">
+            <input type="text" name="introduction" value="{{ $video['introduction'] }}" placeholder="片片在講啥...?">
         </div>
 
         <button class="ui button" type="submit">提交</button>
