e816d2a78c06ed495ad4350fcbf683a37616e3f9
PuddingPudding <hwsh70119@gmail.com>
2017-01-11 20:44:48 +0800

    "新增影片" 之頁面，做了些許美化

--- resources/views/video/create.blade.php
+++ resources/views/video/create.blade.php
@@ -7,34 +7,36 @@
 @endsection
 @section('content')
 
-    <form class="ui form" action="{{route('videoStore')}}" method="post">
-        {{ csrf_field() }}
-        <div class="field">
-            <label style="color: white">片名</label>
-            <input type="text" name="video_name" placeholder="片片名稱...?">
-        </div>
+    <div class="ui segment">
+        <form class="ui form" action="{{route('videoStore')}}" method="post">
+            {{ csrf_field() }}
+            <div class="field">
+                <label style="color: black">片名</label>
+                <input type="text" name="video_name" placeholder="片片名稱...?">
+            </div>
 
-        <div class="field">
-            <label style="color: white">國家</label>
-            <input type="text" name="country" placeholder="國家名稱...?">
-        </div>
+            <div class="field">
+                <label style="color: black">國家</label>
+                <input type="text" name="country" placeholder="國家名稱...?">
+            </div>
 
-        <div class="field">
-            <label style="color: white">片長</label>
-            <input type="text" name="running_time" placeholder="片長多久...?">
-        </div>
+            <div class="field">
+                <label style="color: black">片長</label>
+                <input type="text" name="running_time" placeholder="片長多久...?">
+            </div>
 
-        <div class="field">
-            <label style="color: white">URL</label>
-            <input type="text" name="URL" placeholder="(youtube only)">
-        </div>
+            <div class="field">
+                <label style="color: black">URL</label>
+                <input type="text" name="URL" placeholder="(youtube only)">
+            </div>
 
-        <div class="field">
-            <label style="color: white">簡介</label>
-            <input type="text" name="introduction" placeholder="片片在講啥...?">
-        </div>
+            <div class="field">
+                <label style="color: black">簡介</label>
+                <input type="text" name="introduction" placeholder="片片在講啥...?">
+            </div>
 
-        <button class="ui button" type="submit">提交</button>
-    </form>
+            <button class="ui button" type="submit">提交</button>
+        </form>
+    </div>
 
 @endsection
