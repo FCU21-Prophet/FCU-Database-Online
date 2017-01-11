cbaa50769da630d3a8fccfa04ae8b2e896dea27d
michael <arialove841120@gmail.com>
2017-01-11 20:42:02 +0800

    VideoController - 新增store()內容必須正常 - 可以加入資料 - 沒有防呆(網址)

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Video;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Redirect;
 
 use App\Http\Requests;
 
@@ -13,15 +15,18 @@ class VideoController extends Controller
     public function index()
     {
         $videos = Video::all();
+
         return view('video.list', compact('videos'));
     }
+
     public function show($id)
     {
         $output = Video::findOrFail($id);
         // https://www.youtube.com/embed/nDtj-2qnTXk
-        $strTemp = substr($output['url'] , 32 - strlen($output['url']) );
-        $output['url'] = "https://www.youtube.com/embed/".$strTemp;
-        return view('video.index', ['video' =>  $output]);
+        $strTemp = substr($output['url'], 32 - strlen($output['url']));
+        $output['url'] = "https://www.youtube.com/embed/" . $strTemp;
+
+        return view('video.index', ['video' => $output]);
     }
 
     public function create()
@@ -31,6 +36,14 @@ class VideoController extends Controller
 
     public function store()
     {
-        dd("hello world");
+        $video = new Video;
+        $video->name = Input::get('video_name');
+        $video->country = Input::get('country');
+        $video->running_time = Input::get('running_time');
+        $video->url = Input::get('URL');
+        $video->introduction = Input::get('introduction');
+        $video->save();
+
+        return Redirect::to('video');
     }
 }
