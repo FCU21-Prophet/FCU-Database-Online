f34157cad75a4833e58dc4ad662476bfb81b2639
PuddingPudding <hwsh70119@gmail.com>
2017-01-11 19:00:52 +0800

    新增 "新增影片" 用的頁面，目前是有東西但沒效果的頁面

--- resources/views/video/create.blade.php
+++ resources/views/video/create.blade.php
@@ -7,7 +7,60 @@
 @endsection
 @section('content')
 
-    <button class="ui button">修改</button>
-    <button class="ui button">刪除</button>
+    <table class="ui celled table">
 
+        <thead>
+        <tr>
+            <th>片名</th>
+            <td>
+                <div class="ui action input">
+                    <input type="text" placeholder="片片名稱...?" size=40>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <th>國家</th>
+            <td>
+                <div class="ui action input">
+                    <input type="text" placeholder="國家名稱...?" size=40>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <th>片長(秒)</th>
+            <td>
+                <div class="ui action input">
+                    <input type="text" placeholder="片長多久...?" size=40>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <th>URL (youtube only)</th>
+            <td>
+                <div class="ui action input">
+                    <input type="text" size=100>
+                </div>
+            </td>
+        </tr>
+
+        <tr>
+            <th>簡介</th>
+            <td>
+                <div class="ui action input">
+                    <input type="text" placeholder="這片片在講什麼...?" size=100>
+                </div>
+            </td>
+        </tr>
+        </thead>
+    </table>
+
+    <div class="ui right aligned grid">
+        <div class="right floated left aligned two wide column">
+            <button class="ui button">提交</button>
+        </div>
+    </div>
+    
 @endsection
